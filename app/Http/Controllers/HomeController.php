34839f359f0ee3289bcfdccb3144e4a14b26a8f6
roman08 <rmcentinela@gmail.com>
2017-10-23 23:17:40 -0500

    metodo cosulta procedimuento alamcenado mysql laravel pdo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,34 @@ class HomeController extends Controller
      */
     public function index()
     {
+        /* 
+            // $db = DB::connection();
+        $mysqli = new PDO('mysql:host=localhost;dbname=servicios', "root", "");
+       
+
+
+//$mysqli = DB::connection();
+//dd($mysqli);
+$procInput1 = 123;
+$procInput2 = 456;
+$procInput3 = 789;
+
+//$mysqli = new mysqli();
+
+$call = $mysqli->prepare('CALL test_proc(:uno, :dos, :tres, @sum, @product, @average)');
+$call->bindParam(':uno', $procInput1, PDO::PARAM_INT);
+$call->bindParam(':dos', $procInput2, PDO::PARAM_INT);
+$call->bindParam(':tres', $procInput3, PDO::PARAM_INT);
+$call->execute();
+
+$select = $mysqli->query('SELECT @sum, @product, @average');
+//dd($call);
+$result = $select->fetch(PDO::FETCH_ASSOC);
+$procOutput_sum     = $result['@sum'];
+$procOutput_product = $result['@product'];
+$procOutput_average = $result['@average'];
+dd($result);
+        */
         return view('home');
     }
 }
